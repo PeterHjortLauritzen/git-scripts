6a5fb7cc5c68298a45ec7c146d3584bea7f16772
PeterHjortLauritzen <pel@ucar.edu>
2017-06-29 15:54:28 -0600

    updates

--- ncl_scripts/tke/tke.sh
+++ ncl_scripts/tke/tke.sh
@@ -2,9 +2,20 @@
 #
 # This is a driver script for tke.ncl
 #
+if ( "$#argv" != 2) then
+  echo "Wrong number of arguments specified:"
+  echo "  -arg 1 is run case"
+  echo "  -arg 2 is history file number (e.g., h0)"
+  exit
+endif
 echo "$NCAR_HOST"
 if ("$NCAR_HOST" == "cheyenne") then
   echo "Known computer"
   set ncl_dir = "/glade/u/home/pel/git-scripts/ncl_scripts/tke/"
 endif
-ncl 'dir="/glade2/scratch2/pel/QPC4_with-user-topo_ne30_ne30_pe1152_6nmonths_Co92/run"' 'case="QPC4_with-user-topo_ne30_ne30_pe1152_6nmonths_Co92"' $ncl_dir/tke.ncl
+
+set n = 1
+set case = "$argv[$n]" 
+set n = 2
+set h_number = "$argv[$n]" 
+ncl 'dir="'$PWD'"' 'case="'$case'"' 'h_number="'$h_number'"' $ncl_dir/tke.ncl
