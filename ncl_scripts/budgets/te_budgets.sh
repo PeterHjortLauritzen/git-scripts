c1bb45ff519a064d2af928c679d314ada2b7f69b
Peter Lauritzen <pel@ucar.edu>
2018-09-11 13:29:33 -0600

    mods

--- ncl_scripts/budgets/te_budgets.sh
+++ ncl_scripts/budgets/te_budgets.sh
@@ -6,12 +6,13 @@ if ( "$#argv" != 1) then
 endif
 set n = 1
 set file = "$argv[$n]" 
-if (`hostname` == "hobart.cgd.ucar.edu") then
-  set data_dir = "/scratch/cluster/$USER/"
-  set ncl_dir = "/home/$USER/pel-git-scripts/ncl_scripts/budgets"
-else
+
+if (`hostname` == "cheyenne.ucar.edu") then
   set data_dir = "/glade2/$USER/pel/"
   set ncl_dir = "/glade/u/home/$USER/git-scripts/ncl_scripts/budgets"
+else
+  set data_dir = "/scratch/cluster/$USER/"
+  set ncl_dir = "/home/$USER/git-scripts/ncl_scripts/budgets"
 endif
 echo $file
 if (! -e atm_in) then
