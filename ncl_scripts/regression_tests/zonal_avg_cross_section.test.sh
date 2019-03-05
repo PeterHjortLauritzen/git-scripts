fed43a74777339230f8fd5b8acee39112983388b
PeterHjortLauritzen <pel@izumi.unified.ucar.edu>
2019-03-05 15:23:48 -0700

    some mods

--- ncl_scripts/regression_tests/zonal_avg_cross_section.test.sh
+++ ncl_scripts/regression_tests/zonal_avg_cross_section.test.sh
@@ -10,3 +10,5 @@ source ../zonal_avg_cross_section.sh FV data/zonal_avg_cross_section.fv.nc U
 mv zonal_avg_cross_section.U.pdf $out_dir/
 source ../zonal_avg_cross_section.sh FV SE data/zonal_avg_cross_section.fv.nc data/zonal_avg_cross_section.se.nc U
 mv zonal_avg_cross_sections.U.pdf $out_dir/
+#source ../zonal_avg_cross_section.sh SE1 SE2 data/zonal_avg_cross_section.se.nc data/zonal_avg_cross_section.se2.nc U
+#mv zonal_avg_cross_section.U.pdf $out_dir/
