f7dd9d45ad63752f8271643fa0e6f99526d42997
PeterHjortLauritzen <pel@ucar.edu>
2019-05-21 17:10:28 -0600

    improve TKE scripts

--- ncl_scripts/tke/plot-tke.sh
+++ ncl_scripts/tke/plot-tke.sh
@@ -14,11 +14,11 @@ if ( "$#argv" == 1) then
   set n = 1
   set case = "$argv[$n]" 
   echo "Plotting on one TKE from run directory"
-  ncl 'dir="'$scratch_dir/$case'/run"' 'case="'$case'"' 'ls_option=False' $ncl_dir/plot-tke.ncl
+  ncl 'dir="'$scratch_dir/$case'/run"' 'case="'$case'"' 'ls_option=False' $ncl_dir/plot-tke-new.ncl
 endif
 if ("$#argv" == 0) then
   echo "Plotting all files in in 'ls *.tke_200hPa.nc' in the current directory"
-  ncl 'dir="'$PWD'"' 'ls_option=True' $ncl_dir/plot-tke.ncl
+  ncl 'dir="'$PWD'"' 'ls_option=True' $ncl_dir/plot-tke-new.ncl
 endif
 if ("$#argv" > 1) then
   echo "wrong number of arguments specified:"
