f5a3f2c5fa77f5c9160c71c21ca9bc822768901c
PeterHjortLauritzen <pel@izumi.unified.ucar.edu>
2019-07-27 08:06:54 -0600

    support for low res CONUS

--- run-cam/collection/CONUS.sh
+++ run-cam/collection/CONUS.sh
@@ -10,29 +10,44 @@ setenv PBS_ACCOUNT NACM0003
 #
 # source code (assumed to be in /glade/u/home/$USER/src)
 #
-set src="opt-se-cslam"
+set src="opt-se-cslam-pgf"
 
 #set res="ne0CONUSne30x8_ne0CONUSne30x8_mg17"
 #set res="ne0CONUSne30x8_ne0CONUSne30x8_mt12"
-set res="ne30_ne30_mg17"
-
+#set res="ne30_ne30_mg17"
+set res="ne0TESTONLYne5x4_ne0TESTONLYne5x4_mg37"
 #set cset="FWHIST"
-set cset="FW2000climo"
+#set cset="FW2000climo"
 #set cset="F2000climo"
 #set cset="FHS94"
+set cset="FADIAB"
 #
 # location of initial condition file (not in CAM yet)
 #
-set inic="/glade/p/cgd/amp/pel/inic"
-set walltime="00:55:00"
-set pecount="1800"
+if(`hostname` == 'izumi.unified.ucar.edu') then
+  set homedir="/home"
+  set scratch="/scratch/cluster"
+  set walltime="00:55:00"
+  set pecount="192"  
+#  set queue="monster"
+  set queue="verylong"  
+else
+  set homedir="/glade/u/home"
+  set inic="/glade/p/cgd/amp/pel/inic"
+  set scratch="/glade/scratch"  
+  set walltime="00:55:00"
+  set pecount="1800"
+  set queue="regular"  
+endif
+
+
 set NTHRDS="1"
 set stopoption="ndays"
 set steps="10"
 
 set caze=${src}_${cset}_${res}_${pecount}_NTHRDS${NTHRDS}_${steps}${stopoption}
-/glade/u/home/$USER/src/$src/cime/scripts/create_newcase --case /glade/scratch/$USER/$caze --compset $cset --res $res  --q regular --walltime $walltime --pecount $pecount  --project $PBS_ACCOUNT --run-unsupported
-cd /glade/scratch/$USER/$caze
+$homedir/$USER/src/$src/cime/scripts/create_newcase --case $scratch/$USER/$caze --compset $cset --res $res  --q $queue --walltime $walltime --pecount $pecount  --project $PBS_ACCOUNT --run-unsupported
+cd $scratch/$USER/$caze
 ./xmlchange STOP_OPTION=$stopoption,STOP_N=$steps
 ./xmlchange DOUT_S=FALSE
 ./xmlchange NTHRDS=$NTHRDS
@@ -46,19 +61,29 @@ if ($cset == "ne0CONUSne30x8_ne0CONUSne30x8_mt12") then
   ./xmlchange LND_DOMAIN_FILE=domain.lnd.ne0CONUSne30x8_tx0.1v2.171010.nc  
   ./xmlchange EPS_AAREA=9.0e-6
 endif
-
+if ($cset == "FADIAB") then
+  ./xmlchange --append CAM_CONFIG_OPTS="-analytic_ic -nlev 32"
+  echo "ndens=1 nhtfrq=1 mfilt=10 hfilename_spec='h%t.nc'"   >> user_nl_cam
+  echo "analytic_ic_type ='moist_baroclinic_wave_dcmip2016'" >> user_nl_cam
+  echo "state_debug_checks=.true. "                          >> user_nl_cam
+endif
 if ($cset == "FHS94") then
   ./xmlchange CAM_CONFIG_OPTS="-phys held_suarez -nlev 70" #very important: otherwise you get PS=1000hPa initial condition
 endif
 
 ./case.setup
 
-echo "fincl1             = 'OMEGA500','OMEGA850','PTTEND','FT','ABS_dPSdt'" >> user_nl_cam
-echo "interpolate_output = .true.,.true."              	                    >> user_nl_cam
-echo "inithist           = '6-HOURLY'"   >> user_nl_cam
+#echo "fincl1             = 'OMEGA500','OMEGA850','PTTEND','FT','ABS_dPSdt'" >> user_nl_cam
+#echo "interpolate_output = .true.,.true."              	                    >> user_nl_cam
+#echo "inithist           = '6-HOURLY'"   >> user_nl_cam
 echo "se_statefreq       = 1"                                             >> user_nl_cam
+
 # echo "ncdata = '/glade/p/nsc/nacm0003/input/CAM-SE/FW2000climo_conus_30_x8_c190613.nc'"   >> user_nl_cam
 # echo "bnd_topo = '/glade/p/cgd/amp/pel/topo/conus_30_x8_nc3000_Co060_Fi001_MulG_PF_nullRR_Nsw042.C60-repaired.nc'">>user_nl_cam
 # echo "use_topo_file      =  .true.   ">>user_nl_cam
-qcmd -- ./case.build
+if(`hostname` == 'izumi.unified.ucar.edu') then
+  ./case.build
+else
+  qcmd -- ./case.build
+endif  
 ./case.submit
